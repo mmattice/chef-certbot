4611fb60747e2c27717b9303fedcf28eb9ba0208
Mike Mattice <mmattice@hoegg.software>
2017-08-18 14:39:32 -0500

    support 17.04 and 14.04 also

--- metadata.rb
+++ metadata.rb
@@ -1,22 +1,22 @@
 name 'certbot'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Mike Mattice'
+maintainer_email 'mmattice@hoegg.software'
+license 'apache-2.0'
 description 'Installs/Configures certbot'
 long_description 'Installs/Configures certbot'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.9' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/certbot/issues'
+issues_url 'https://github.com/hoeggsoftware/chef-certbot/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/certbot'
+source_url 'https://github.com/hoeggsoftware/chef-certbot'
 
 depends 'yum-epel'
